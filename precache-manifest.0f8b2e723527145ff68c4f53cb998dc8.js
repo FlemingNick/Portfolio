43f3eae94ade5646533e506234e65ca636d93a7c
Jan-Fleming Nickelsen <j.nickelsen@hmmh.de>
2020-10-12 08:34:12 +0200

    Updates

---
+++ precache-manifest.0f8b2e723527145ff68c4f53cb998dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9c81cd17e1aedbde33ae5caffa32b5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "cf6998da1c5196601882",
+    "url": "/Portfolio/static/css/main.c5548a41.chunk.css"
+  },
+  {
+    "revision": "7b82f77dc250a2ee8784",
+    "url": "/Portfolio/static/js/2.3030a65e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.3030a65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6998da1c5196601882",
+    "url": "/Portfolio/static/js/main.fc19a576.chunk.js"
+  },
+  {
+    "revision": "2ac93e3e79e0661d7a8a",
+    "url": "/Portfolio/static/js/runtime-main.d35d9740.js"
+  }
+]);
